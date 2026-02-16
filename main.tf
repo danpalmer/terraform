9f9dad50e71a7f4606568eb8e126ab7796f6fcc7
Dan Palmer <dan@danpalmer.me>
2026-02-16 19:26:56 +1100

    Set up pages deployment

--- main.tf
+++ main.tf
@@ -8,6 +8,6 @@ terraform {
 }
 
 provider "cloudflare" {
-  email   = var.cloudflare_email
-  api_key = var.cloudflare_api_key
+  email     = var.cloudflare_email
+  api_token = var.cloudflare_api_key
 }
