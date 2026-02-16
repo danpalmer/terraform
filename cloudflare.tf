3b07e7d321454987b78419a297b05b0eb518760e
Dan Palmer <dan@danpalmer.me>
2026-02-16 19:41:04 +1100

    Update to add zero trust team

---
+++ cloudflare.tf
@@ -0,0 +1,34 @@
+resource "cloudflare_pages_project" "danpalmer-me" {
+  account_id        = var.cloudflare_account_id
+  name              = "danpalmer-me"
+  production_branch = "master"
+  build_config = {
+    build_caching   = false
+    build_command   = "hugo --minify -b $CF_PAGES_URL"
+    destination_dir = "public"
+    root_dir        = ""
+  }
+  source = {
+    type = "github"
+    config = {
+      owner             = "danpalmer"
+      owner_id          = "202400"
+      repo_name         = "danpalmer.me"
+      repo_id           = "119259629"
+      production_branch = "master"
+
+      production_deployments_enabled = true
+      preview_deployment_setting     = "all"
+      pr_comments_enabled            = true
+      path_includes                  = ["*"]
+      preview_branch_includes        = ["*"]
+    }
+  }
+}
+
+resource "cloudflare_zero_trust_organization" "danpalmer-team" {
+  account_id      = var.cloudflare_account_id
+  name            = "Dan Palmer"
+  auth_domain     = "danpalmer.cloudflareaccess.com"
+  is_ui_read_only = true
+}
